b4b13e6bfd25dcea0f426d59492e902a3fce8f6a
andresjesusu <andresjesusu@gmail.com>
2022-02-24 13:06:21 -0300

    primera entrega

---
+++ .js
@@ -0,0 +1,72 @@
+// let user = 'Andres';
+
+// let user2 = 'Pedro';
+
+// user2 = user  //Copia Por valor
+
+// console.log(user, user2)
+
+// let auto = {marca: 'Toyota'};
+
+// let auto2 
+
+// auto2 = auto
+
+// auto2.marca = 'Honda';
+// console.log(auto, auto2)
+
+// let mensaje = `asduhaksuhdasd${auto2} askhdiuashdiasda`
+
+// console.log(mensaje)
+
+// function mostrarLista (miLista) {
+
+//     if (miLista.length === 0)
+//     console.log('Lista vacia')
+    
+//     else console.log(miLista)
+// }
+
+
+// (function (miLista) {
+//     if (miLista.length === 0)
+//     console.log('Lista vacia')
+    
+//     else console.log(miLista)
+
+// })([]);
+
+// function multiplicador(a) {
+
+//     return (function (b) {
+//         console.log(a * b)
+
+//     })(2);
+
+// }
+// multiplicador(5)
+
+// let miCalculadora = function(a) {
+    
+    
+//     function duplicar (){
+//         console.log(a*2)
+//     }
+
+//     function triplicar (){
+//         console.log(a*3)
+//     }
+
+//     function multiplicador(b) {
+//         console.log(a*b)
+
+//     }
+
+//     return {duplicar, triplicar, multiplicador}
+
+// }
+
+// miCalculadora(4).duplicar()
+// miCalculadora(4).triplicar()
+// miCalculadora(4).multiplicador(4)
+
